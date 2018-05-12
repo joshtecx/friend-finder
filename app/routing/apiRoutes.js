335ce16a0a273f65f8d5aeef469afbbecc8d6eae
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-05-11 23:37:22 -0400

    more updates. Will not be finished before class

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,6 +14,31 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", function(request, response){
+        const newFriend = request.body;
+        let totalDifference;
+        let lowestDifference;
+        let newBestFriend;
+        
+        
+        // console.log(newFriend);
+
+        for(let i = 0; i < friendData.length; i++){
+            // console.log(friendData[i].scores);
+            totalDifference = 0;
+            for(let j = 0; j < friendData[i].scores[j]; j++){
+                // console.log(friendData[i].scores[j]);
+                totalDifference += Math.abs(parseInt(newFriend.scores[j] - friendData[i].scores[j]));
+                // console.log(totalDifference);
+                
+            }
+        }
+
+        lowestDifference = Math.min(totalDifference);
+        console.log(lowestDifference);
+
+        
+        // friendData.push(newFriend);
+        // response.json();
         
     });
 };
